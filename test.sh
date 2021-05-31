82a44b37cac17918fe2833b0eaf11dca0ed480c9
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-01 02:04:28 +0900

    add gitlab-ci, confirm test runs locally

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 # test linting (test syntax, format code with prettier)
-yarn lint && echo "Linitng passed!" || "Linting error, check syntax!" && exit 1
-yarn test:unit && echo "unit test passed!" || "Linting error, check syntax!" && exit 1
+yarn lint && echo "Linitng passed!" || ("Linting error, check syntax!" && exit 1)
+# unit test
+yarn test:unit && echo "unit test passed!" || ("error in unit test!" && exit 1)
