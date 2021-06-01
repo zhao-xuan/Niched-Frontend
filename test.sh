412e1b0b5d70a10bbaea079b0b4d326b336d323b
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-01 22:10:27 +0900

    add unit test to gitlab-ci

--- test.sh
+++
@@ -1,4 +0,0 @@
-# test linting (test syntax, format code with prettier)
-yarn lint && echo "Linitng passed!" || ("Linting error, check syntax!" && exit 1)
-# unit test
-yarn test:unit && echo "unit test passed!" || ("error in unit test!" && exit 1)
