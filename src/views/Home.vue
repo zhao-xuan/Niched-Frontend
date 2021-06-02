d796090fa8205d7a2045456f88c87b7069fbbd18
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-03 02:25:32 +0900

    implement topbar ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,24 @@
 <template>
-  <el-menu
-    :default-active="activeIndex2"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#8a00e6"
-    text-color="#fff"
-    active-text-color="#ffd04b"
-  >
-    <el-col :span="16" class="bg-purple">
-      <div style="margin: 30px">niched</div></el-col
-    >
-
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-submenu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-submenu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="4"
-      ><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item
-    >
-  </el-menu>
+  <Topbar @input="onSearchSpace" />
   <Dashboard />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Dashboard from "./dashboard/Dashboard.vue";
+import Topbar from "./Topbar.vue";
 
 export default defineComponent({
   name: "Home",
-  components: { Dashboard },
+  components: { Dashboard, Topbar },
+  setup() {
+    const onSearchSpace = (searchString: string) => {
+      //TODO: fetch spaces
+      console.log(searchString);
+    };
+    return {
+      onSearchSpace,
+    };
+  },
 });
 </script>
---
+++ src/views/Topbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"
+        ><h1 class="text-light ml-0 pl-0">niched</h1></a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <input
+          class="form-control ml-sm-5 mr-sm-5"
+          type="text"
+          placeholder="Search"
+          @input="onChange"
+        />
+        <div class="mr-sm-5">
+          <button class="btn btn-outline-success">create space</button>
+        </div>
+        <div>
+          <el-avatar icon="el-icon-user-solid"></el-avatar>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  emits: ["input"],
+  setup(_, { emit }) {
+    const onChange = (e: Event) => {
+      emit("input", (e.target as HTMLInputElement).value);
+    };
+    return { onChange };
+  },
+});
+</script>
