9d62d99149d1f02e66a1867436e8ffb4631a613c
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-04 13:32:50 +0900

    add hooks

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { ref, Ref, UnwrapRef, watchEffect } from "vue";
+
+export const useFetch = <Response>(
+  request: () => Promise<Response>,
+  immediate = false
+): {
+  data: Ref<UnwrapRef<Response> | null>;
+  fetching: Ref<boolean>;
+  fetched: Ref<boolean>;
+  error: Ref<any>;
+} => {
+  const data = ref<Response | null>(null);
+  const fetching = ref<boolean>(false);
+  const fetched = ref<boolean>(false);
+  const error = ref<any>(null);
+
+  const doFetch = async () => {
+    fetching.value = true;
+    try {
+      data.value = (await request()) as UnwrapRef<Response>;
+      fetched.value = true;
+    } catch (err) {
+      error.value = err;
+    }
+    fetching.value = false;
+  };
+
+  watchEffect(() => {
+    if (!immediate) return;
+    doFetch();
+  });
+  return { data, fetching, fetched, error };
+};
