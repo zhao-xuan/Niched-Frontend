057ea442db0921ce82480223a463a829b6c68c0a
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-04 13:03:22 +0100

    Configure environments for local dev and cloud deployment

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://niched-api.herokuapp.com";
-// export const API_SERVER = 'http://localhost:3000/'
+export const SERVER_URL = process.env.VUE_APP_SERVER_URL
+console.log(process.env)
