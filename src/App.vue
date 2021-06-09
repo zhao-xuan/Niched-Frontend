10a7aa50021bd2642dd08476f6b7c6107af90942
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-09 15:24:38 +0900

    use alice as default user

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
-  <router-view />
+  <StateProvider :state="state">
+    <router-view />
+  </StateProvider>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import StateProvider from "./StateProvider.vue";
+import { State } from "./state";
+import { useUser } from "@/hooks/useUser";
+
+export default defineComponent({
+  name: "App",
+  components: { StateProvider },
+  setup() {
+    const { userName, email, subscribedGroups, interests } = useUser(
+      "alice",
+      true
+    );
+    const state: State = reactive({
+      loggedIn: true,
+      userName,
+      email,
+      subscribedGroups,
+      interests,
+    });
+
+    return { state };
+  },
+});
+</script>
 
 <style>
 body {
