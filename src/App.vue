c3d59f597c36b01109565dcc167ed9cef018061f
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-09 17:27:53 +0900

    set defautl user none

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,17 @@
 import { defineComponent, reactive } from "vue";
 import StateProvider from "./StateProvider.vue";
 import { State } from "./state";
-import { useUser } from "@/hooks/useUser";
 
 export default defineComponent({
   name: "App",
   components: { StateProvider },
   setup() {
-    const { userName, email, subscribedGroups, interests } = useUser(
-      "alice",
-      true
-    );
-    const state: State = reactive({
-      loggedIn: true,
-      userName,
-      email,
-      subscribedGroups,
-      interests,
-    });
+    const state: State = {
+      loggedIn: false,
+      userName: "",
+      subscribedGroups: [],
+      interests: [],
+    };
 
     return { state };
   },
