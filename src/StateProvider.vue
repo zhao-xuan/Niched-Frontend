5a4f8c6c29c46293c54ee903c67e8d1637ad52c1
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-09 15:19:34 +0900

    add StateProvider

---
+++ src/StateProvider.vue
@@ -0,0 +1,20 @@
+<template>
+  <slot />
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from "vue";
+import { State, provideState } from "./state";
+export default defineComponent({
+  name: "ProvideUser",
+  props: {
+    state: {
+      required: true,
+      type: Object as PropType<State>,
+    },
+  },
+  setup(props) {
+    provideState(props.state);
+  },
+});
+</script>
