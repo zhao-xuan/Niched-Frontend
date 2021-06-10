085de4e4bd4fe0e8ef13952075d2b37aee962c9b
Gavin <gjw19@ic.ac.uk>
2021-06-10 12:33:22 +0100

    Completed forms for event and post creation in terms of UI

--- src/views/space/Space.vue
+++ src/views/space/Space.vue
@@ -1,82 +1,204 @@
 <template>
+  <!-- <TopBar /> -->
   <div class="row py-5 px-4">
-    <div class="col-md-5 mx-auto">
+    <div class="col-md-10 mx-auto">
       <!-- Profile widget -->
-      <div class="bg-white shadow rounded overflow-hidden mt-3s">
+      <div class="bg-light shadow rounded overflow-hidden mt-3s">
         <div
-          class="px-4 pt-0 pb-5 cover"
-          :style="{
-            backgroundImage: 'url(' + imageUrl + ')',
-          }"
+          class="px-4 pt-0 pb-5 cover rounded"
+          style="
+            background-image: url('https://iamafoodblog.b-cdn.net/wp-content/uploads/2021/04/mayo-ramen-2638w.jpg');
+            background-position: center;
+            height: 300px;
+          "
         >
           <div class="mb-2 text-white d-flex justify-content-center">
             <div class="px-4">
-              <h2 class="pt-5 my-4">
-                {{ name }}
-              </h2>
+              <!-- <h2 class="pt-5 my-4">
+                {{ "Apple WWDC Event Watching Session" }}
+              </h2> -->
             </div>
-            <p class="small mb-4"></p>
           </div>
         </div>
-        <div class="media align-items-end profile-head">
-          <div class="profile m-3">
-            <a href="#" class="btn btn-dark btn-sm btn-block"
-              >Join the space!</a
-            >
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="px-4 pt-3 d-flex text-left">
+              <h2>{{ name }}</h2>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="px-4 pt-3 d-flex text-left">
+              <p>{{ description }}</p>
+            </div>
+          </div>
+          <div class="col-sm-2">
+            <div class="px-4 py-3 d-flex justify-content-end text-center">
+              <ul class="list-inline mb-0">
+                <li class="list-inline-item">
+                  <h5 class="font-weight-bold mb-0 d-block">40</h5>
+                  <small class="text-muted">
+                    <i class="fas fa-user mr-1"></i>Members</small
+                  >
+                </li>
+                <li class="list-inline-item">
+                  <h5 class="font-weight-bold mb-0 d-block">23</h5>
+                  <small class="text-muted">
+                    <i class="fas fa-user mr-1"></i>Posts</small
+                  >
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div class="bg-light p-4 d-flex justify-content-end text-center">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item">
-              <h5 class="font-weight-bold mb-0 d-block">40</h5>
-              <small class="text-muted">
-                <i class="fas fa-user mr-1"></i>Members</small
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="px-4 py-3">
-          <h5 class="mb-0">About</h5>
-          <div class="p-4 rounded shadow-sm bg-light">
-            <p class="font-italic mb-0">{{ description }}</p>
-            <p class="font-italic mb-0">{{ description }}</p>
-            <p class="font-italic mb-0">{{ description }}</p>
+
+        
+        <div class="row pb-4 px-4">
+          <div class="col-md-8 order-0">
+            <el-card style="margin: 20px auto; background-color: #FFE8E0;">
+              <template #header>
+                <div>
+                  <span>
+                    <a href="https://www.google.co.uk">
+                      <b><font color="#FF7744">Event: </font>Let’s go to Ichiran Ramen!</b>
+                    </a>
+                  </span>
+                  <el-button type="text" style="float: right; margin-top: -10px"
+                    >Sunday 15/06/2021 @ 12:00PM</el-button
+                  >
+                </div>
+              </template>
+              <div class="text item">
+                There is a new item in the Ichiran ramen shop: Yakikamo. Sounds good. Anyone interested? 
+                We can go together. Meeting at the Imperial College Central Library. Anyone is welcome to 
+                join, and please remember to bring you’re friends along too! Hoping to get over 10 people 
+                this time!
+              </div>
+            </el-card>
+
+            <el-card style="margin: 20px auto">
+              <template #header>
+                <div>
+                  <span><b>What’s your favourite Ramen shop in London?</b></span>
+                  <el-button type="text" style="float: right; margin-top: -10px"
+                    >More</el-button
+                  >
+                </div>
+              </template>
+              <div class="text item">
+                There is a new item in the Ichiran ramen shop: Yakikamo. Sounds good. Anyone interested?
+                We can go together. Meeting at the Imperial College Central Library. Anyone is welcome to
+                join, and please remember to bring you’re friends along too! Hoping to get over 10 people
+                this time! Please remember to bring you’re friends along and...
+              </div>
+            </el-card>
+
+            <el-card style="margin: 20px auto">
+              <template #header>
+                <div>
+                  <span><b>Ramen really is better in Japan than in London! Today I found out that...</b></span>
+                  <el-button type="text" style="float: right; margin-top: -10px"
+                    >More</el-button
+                  >
+                </div>
+              </template>
+              <div class="text item">
+                There is a new item in the Ichiran ramen shop: Yakikamo. Sounds good. Anyone interested?
+                We can go together. Meeting at the Imperial College Central Library. Anyone is welcome to
+                join, and please remember to bring you’re friends along too! Hoping to get over 10 people
+                this time! Please remember to bring you’re friends along and...
+              </div>
+            </el-card>
+
+            <el-card style="margin: 20px auto">
+              <template #header>
+                <div>
+                  <span><b>My friend Leo’s birthday is coming up soon! What do you recommend?</b></span>
+                  <el-button type="text" style="float: right; margin-top: -10px"
+                    >More</el-button
+                  >
+                </div>
+              </template>
+              <div class="text item">
+                There is a new item in the Ichiran ramen shop: Yakikamo. Sounds good. Anyone interested?
+                We can go together. Meeting at the Imperial College Central Library. Anyone is welcome to
+                join, and please remember to bring you’re friends along too! Hoping to get over 10 people
+                this time! Please remember to bring you’re friends along and...
+              </div>
+            </el-card>
+            
+            <el-card style="margin: 20px auto; background-color: #FFE8E0;">
+              <template #header>
+                <div>
+                  <span>
+                    <a href="https://www.google.co.uk">
+                      <b><font color="#FF7744">Event: </font>Want to try Chinese DanDan Noodle too?</b>
+                    </a>
+                  </span>
+                  <el-button type="text" style="float: right; margin-top: -10px"
+                    >Sunday 15/06/2021 @ 12:00PM</el-button
+                  >
+                </div>
+              </template>
+              <div class="text item">
+                There is a new item in the Ichiran ramen shop: Yakikamo. Sounds good. Anyone interested? 
+                We can go together. Meeting at the Imperial College Central Library. Anyone is welcome to 
+                join, and please remember to bring you’re friends along too! Hoping to get over 10 people 
+                this time!
+              </div>
+            </el-card>
+
           </div>
-        </div>
-        <div class="py-4 px-4">
-          <div class="d-flex align-items-center justify-content-between mb-3">
-            <h5 class="mb-0">Recent events</h5>
-            <a href="#" class="btn btn-link text-muted">Show all</a>
-          </div>
-          <div class="row">
-            <div class="col-lg-6 mb-2 pr-lg-1">
-              <img
-                src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                alt=""
-                class="img-fluid rounded shadow-sm"
-              />
-            </div>
-            <div class="col-lg-6 mb-2 pl-lg-1">
-              <img
-                src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                alt=""
-                class="img-fluid rounded shadow-sm"
-              />
-            </div>
-            <div class="col-lg-6 pr-lg-1 mb-2">
-              <img
-                src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                alt=""
-                class="img-fluid rounded shadow-sm"
-              />
-            </div>
-            <div class="col-lg-6 pl-lg-1">
-              <img
-                src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                alt=""
-                class="img-fluid rounded shadow-sm"
-              />
-            </div>
+
+          <div class="col-md-4 order-1">
+            <el-card style="margin: 20px auto">
+              <div>
+                <h3>Create a new Post</h3>
+                <form class="pt-2">
+                  <div class="form-group">
+                    <input type="email" class="form-control" id="postTitle" placeholder="Post Title">
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" id="postDescription" rows="3" placeholder="Post Details"></textarea>
+                  </div>
+                  <div class="form-group" style="float: right;">
+                    <button type="submit" class="btn btn-primary mb-2">Create Post</button>
+                  </div>
+                </form>
+              </div>
+            </el-card>
+
+            <el-card style="margin: 20px auto">
+              <div>
+                <h3>Organise a new Event!</h3>
+                <form class="pt-2">
+                  <div class="form-group">
+                    <input type="email" class="form-control" id="eventTitle" placeholder="Event Title">
+                  </div>
+                  <div class="form-group row">
+                    <label for="example-date-input" class="col-2 col-form-label">Date</label>
+                    <div class="col-10">
+                      <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="example-time-input" class="col-2 col-form-label">Time</label>
+                    <div class="col-10">
+                      <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" id="eventDescription" rows="3" placeholder="Event Details"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" id="eventMemberCount" placeholder="Member Count (Optional)">
+                  </div>
+                  <div class="form-group" style="float: right;">
+                    <button type="submit" class="btn btn-primary mb-2">Create Post</button>
+                  </div>
+                </form>
+              </div>
+            </el-card>
           </div>
         </div>
       </div>
@@ -85,12 +207,14 @@
 </template>
 
 <script lang="ts">
+// import TopBar from "../Topbar.vue";
 import { defineComponent } from "vue";
 import { useSpace } from "@/hooks/useSpace";
 import { useRoute } from "vue-router";
 
 export default defineComponent({
   name: "Space",
+  // components: { TopBar },
   setup() {
     const route = useRoute();
     const groupId = route.params.id as string;
