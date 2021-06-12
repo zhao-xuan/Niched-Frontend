7ec669514bb452ff50c8b8f8f001590b6a09e633
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-13 03:41:18 +0900

    implement thread.ts and useThread

--- src/api/thread.ts
+++ src/api/thread.ts
@@ -1,7 +1,42 @@
 import axios from "axios";
 import { SERVER_URL } from "@/api/constant";
 
-//create event
+export type Thread = {
+  groupId: string;
+  authorId: string;
+  description: string;
+  title: string;
+  threadId: string;
+  creationTime: string;
+};
+
+//fetch a thread
+export type ThreadResponse = {
+  group_id: string;
+  author_id: string;
+  description: string;
+  title: string;
+  thread_id: string;
+  creation_time: string;
+};
+
+//fetch a thread
+export const fetchThread = async (
+  threadId: string
+): Promise<ThreadResponse> => {
+  const res = await axios.get(`${SERVER_URL}/thread/${threadId}`);
+  return res.data;
+};
+
+//fetch threads
+export const fetchThreads = async (
+  groupId: string
+): Promise<ThreadResponse[]> => {
+  const res = await axios.get(`${SERVER_URL}/${groupId}/threads`);
+  return res.data;
+};
+
+//create thread
 
 type ThreadCreation = {
   group_id: string;
@@ -10,10 +45,7 @@ type ThreadCreation = {
   title: string;
 };
 
-export type ThreadCreationResponse = ThreadCreation & {
-  thread_id: string;
-  creation_time: string;
-};
+export type ThreadCreationResponse = ThreadResponse;
 
 export const postThreadCreation = async (
   req: ThreadCreation
