edf3a68f0c535f677039bcca1d5042c63b0f5fe6
xuan-zhao <zhaoxuan0914@hotmail.com>
2021-06-21 18:35:17 +0100

    Change event-card in profile page to be consistent

--- src/views/users/Profile.vue
+++ src/views/users/Profile.vue
@@ -131,6 +131,59 @@
                       v-for="event in eventsJoined.slice(0, 5)"
                       :key="event.eventId"
                       style="cursor: pointer"
+                      @click="$emit('click-event', event.eventId)"
+                    >
+                      <div class="row">
+                        <div
+                          class="text-secondary col-12 col-sm-2 mr-0 pr-0"
+                          style="
+                            font-weight: 500;
+                            font-size: 15px;
+                            min-width: 37px;
+                          "
+                        >
+                          <i class="el-icon-time pr-1"></i
+                          >{{
+                            new Date(event.eventDate).toLocaleString("en-US", {
+                              hour: "numeric",
+                              minute: "numeric",
+                              hour12: true,
+                            })
+                          }}
+                        </div>
+                        <div class="col-12 col-sm-10">
+                          <div v-if="!selfProfile" class="pb-2">
+                            <el-tag
+                              v-if="
+                                event.members.going.includes(loggedInUserName)
+                              "
+                            >
+                              You're also going!
+                            </el-tag>
+                            <el-tag v-else> You're interested in this </el-tag>
+                          </div>
+                          <div>
+                            {{ event.title }}
+                          </div>
+                          <div
+                            class="d-flex justify-content-end"
+                            style="
+                              font-weight: 400;
+                              font-size: 14px;
+                              color: #a6a6a6;
+                            "
+                          >
+                            <i class="el-icon-user pt-1"></i>
+                            <b class="pl-1">{{ event.authorId }}</b>
+                          </div>
+                        </div>
+                      </div>
+                    </card>
+                    <!-- <card
+                      class="mt-3 p-3"
+                      v-for="event in eventsJoined.slice(0, 5)"
+                      :key="event.eventId"
+                      style="cursor: pointer"
                       @click="jumpToEvent(event.groupId, event.eventId)"
                     >
                       <div class="row">
@@ -172,7 +225,7 @@
                           </div>
                         </div>
                       </div>
-                    </card>
+                    </card> -->
                   </div>
                 </div>
               </div>
